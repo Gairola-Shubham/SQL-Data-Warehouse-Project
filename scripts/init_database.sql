17b90d8bfd9e583099ce9e0184f8b95ff577c4e5
Shubham Gairola <shubhamgairola124@gmail.com>
2026-05-11 21:19:13 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,9 @@
+-- create Database 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+-- schema for different layers
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
